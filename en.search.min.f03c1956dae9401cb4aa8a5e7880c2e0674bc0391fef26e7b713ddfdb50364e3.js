12b85e5e9b4703d6623b593e2c4c896e408b812e
ercang <ercang@users.noreply.github.com>
2020-09-30 20:56:36 +0000

    deploy: 27747ceb65b3a4dfde8269ca602d0868bf41d0d7

--- en.search.min.f03c1956dae9401cb4aa8a5e7880c2e0674bc0391fef26e7b713ddfdb50364e3.js
+++ en.search.min.f03c1956dae9401cb4aa8a5e7880c2e0674bc0391fef26e7b713ddfdb50364e3.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.fa4e5d05b8000e924ea897233c9ee10f08b6e266f161d25953880697994ed727.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.5b52b69544f04d351c224d0c03f203ebfdccf58dba9bbe705fe4c3549c3260c0.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
